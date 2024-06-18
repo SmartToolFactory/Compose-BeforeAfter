06653e7a839275697996ec013efeeb24bc54f7d3
Nishant Srivastava <nisrulz@gmail.com>
2024-06-18 18:22:49 +0200

    replaced deprecated code

--- app/src/main/java/com/smarttoolfactory/composebeforeafter/ui/theme/Theme.kt
+++ app/src/main/java/com/smarttoolfactory/composebeforeafter/ui/theme/Theme.kt
@@ -13,7 +13,7 @@ import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
-import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -55,9 +55,14 @@ fun ComposeBeforeAfterTheme(
     }
     val view = LocalView.current
     if (!view.isInEditMode) {
+        val currentWindow =
+            (view.context as? Activity)?.window
+                ?: throw Exception("Not in an activity - unable to get Window reference")
+
         SideEffect {
-            (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
-            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
+            currentWindow.statusBarColor = colorScheme.primary.toArgb()
+            WindowCompat.getInsetsController(currentWindow, view)
+                .isAppearanceLightStatusBars = darkTheme
         }
     }
 
